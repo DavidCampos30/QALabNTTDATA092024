2cf2a684d5e7c8a059fa79604b901970b6b4296f
David <camposgamarra15@gmail.com>
2025-01-30 17:35:45 -0500

    Se agrego la carpeta Java ArrayMulti

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        String configTest[] = {"123.123.42.12", "jkbe", "21231"};
-        String configProd[] = {"www.google.com", "jkbenites", "alalals"};
-        String configDesa[] = {"123.123.42.10", "userdesa02", "1234"};
 
-        int XPROD = 2;
-        int XTEST = 1;
-        int XDESA = 0;
-        int CSERV = 0;
-        int CUSER = 1;
-        int CPASS = 2;
-        int CAMB = 3;
+        int listaArray[][] = { {12,3,3},  {19,2,4} };
 
-        String config[][] = {
-                {"123.123.42.10", "userdesa02", "1234", "DESA", "otro campo"},   //0
-                {"123.123.42.12", "jkbe", "21231", "TEST"},        //1
-                {"www.google.com", "jkbenites", "alalals", "PROD"} //2
-        };
+        //System.out.println(listaArray[1][1]);
 
+        for(int i=0;i<listaArray.length;i++ ){
+            //System.out.println("Hola: " + i);
+            for(int j=0;j< listaArray[i].length;j++){
+                System.out.println(listaArray[i][j]);
 
-        //Hacer doble bucle for/for
-
-
-        //CLAVE: DESARROLLO
-
-        //System.out.println(config[3][1]);
-        //System.out.println(config[1][1]);
-        System.out.println(config[XPROD][CPASS]); //...
-
-
+            }
+            System.out.println("");
+        }
     }
 }
